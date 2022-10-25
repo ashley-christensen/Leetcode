f81db499154b9af8dc194bcd1d9d6fcb512cc98a
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-10-25 14:02:47 -0700

    removed uplicate linked list

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -9,29 +9,22 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+//create prev set to -Infinity before head
+//keep incrementing curr until prev and curr have different values, set prev.next to new current, space complexity is constant O(1)
 var deleteDuplicates = function (head) {
+ let prev = -Infinity
+ prev.next = head;
  let curr = head;
- let prev = head;
 
- while (curr) {
-  if (prev === curr) removeNode(curr);
+ if (prev.val === curr.val) {
+  while (curr && curr.val === prev.val) {
+   curr = curr.next;
+  }
+  prev.next = curr;
+ } else {
   prev = curr;
   curr = curr.next;
+  prev.next = curr.next
  }
-};
 
-function removeNode(node) {
- function get(idx) {
-  let count = 0;
-  let curr = this.head;
-  while (idx !== count) {
-   curr = curr.next;
-   count++;
-  }
-  return current;
- }
- let prev = get(idx - 1);
- let curr = prev.next;
- prev.next = curr.next;
- curr.next = null;
 }
